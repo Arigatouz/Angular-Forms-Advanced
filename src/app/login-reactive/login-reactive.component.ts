62a743cf59339f734901dece31320926903e3442
Ali-Gamal <aga@luftborn.com>
2023-07-24 08:00:11 +0300

    non nullable form builder

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,21 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login-reactive.component.html',
-  styleUrls: ['./login-reactive.component.css']
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  // email: FormControl = new FormControl("", {
+  //   validators: [Validators.required, Validators.email],
+  //   updateOn: "blur",
 
+  // });
+  // password: FormControl = new FormControl("", {
+  //   validators: [
+  //     Validators.required,
+  //     Validators.minLength(8),
+  //     Validators.maxLength(16),
+  //     createPasswordStrengthValidator(),
+  //   ],
+  // });
 
-  constructor() {
+  // form: FormGroup = new FormGroup({
+  //   email: this.email,
+  //   password: this.password,
+  // });
 
+  form = this._formBuilder.group({
+    email: [
+      "",
+      { validators: [Validators.required, Validators.email], updateOn: "blur" },
+    ],
+    password: [
+      "",
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+          createPasswordStrengthValidator(),
+        ],
+      },
+    ],
+  });
 
+  constructor(private _formBuilder: NonNullableFormBuilder) {}
+
+  get email() {
+    return this.form.controls.email;
   }
 
-  ngOnInit() {
-
+  get password() {
+    return this.form.controls.password;
   }
-
+  ngOnInit() {}
+  login() {
+    const formValue = this.form.value;
+  }
+  reset() {
+    this.form.reset();
+    console.log(this.form.value);
+  }
 }
